0b3ee27df1ba5ddd7a83503866bff4fb00d7340b
Vishnu P <pvishnukakkodi@gmail.com>
2024-12-28 17:25:19 +0530

    Error solved

--- Frontend/src/components/header/landingPage.tsx
+++ Frontend/src/components/header/landingPage.tsx
@@ -1,10 +1,60 @@
 import React, { useState } from 'react';
-import SignUpButton from '../buttons/SignUpButton';
 import SignUpModal from '../modals/SignUpModal';
-import LoginButton from '../buttons/LoginButton';
 import LoginModal from '../modals/LoginModal';
 import OtpModal from '../modals/OtpModal';
 import EmailVerify from '../modals/EmailVerify';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'store';
+
+interface LoginButtonProps{
+    label?: string;
+    className?:string;
+    handleLogin?: () => void
+    setModalOpen?: (state: boolean) => void;
+}
+
+const LoginButton: React.FC<LoginButtonProps>=({
+    handleLogin
+}) => {
+    return (
+        <>
+        <button
+        onClick={handleLogin}
+        className={`ml-4 text-[14px] font-[800] capitalize text-black bg-transparent border-2 border-black hover:bg-yellow-500 hover:text-black px-[30px] py-[11px]`}>
+            Log in
+        </button>
+        </>
+    )
+}
+
+interface SignUpButtonProps {
+  label?: string;
+  className?: string;
+  handleSignUp?: () => void; 
+  setModalOpen?: (state: boolean) => void; 
+}
+
+const SignUpButton: React.FC<SignUpButtonProps> = ({ 
+  label = 'Login', 
+  className = '', 
+  handleSignUp, 
+  setModalOpen 
+}) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <>
+      
+      {handleSignUp && (
+        <button
+          onClick={handleSignUp}
+          className={`ml-4 text-[14px] font-medium capitalize text-white bg-black border-2 border-black hover:bg-yellow-500 hover:text-black px-[30px] py-[11px]`}>
+          Sign Up
+        </button>
+      )}
+    </>
+  );
+};
 
 const LandingPageHeader = () => {
   const [isSignUpModalOpen, setSignUpModalOpen] = useState(false);
