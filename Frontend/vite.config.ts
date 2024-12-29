7194435238bdc4394fdfb46d944035200be6aaad
Vishnu P <pvishnukakkodi@gmail.com>
2024-12-29 14:27:44 +0530

    Error solved

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 5000, // Adjust this based on your needs
+    chunkSizeWarningLimit: 5000, 
     rollupOptions: {
       output: {
         manualChunks(id) {
